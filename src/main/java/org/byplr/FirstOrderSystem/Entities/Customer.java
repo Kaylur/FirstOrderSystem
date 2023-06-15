0f527db89065f89b01ebdb11aa39a0e790f0a57d
Khahliso Motsoeneng <khamotso021@student.wethinkcode.co.za>
2023-06-15 18:02:23 +0200

    Customer Entity Testing setup

---
+++ src/main/java/org/byplr/FirstOrderSystem/Entities/Customer.java
@@ -0,0 +1,30 @@
+package org.byplr.FirstOrderSystem.Entities;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 45)
+    private String email;
+
+    @Column(nullable = false, length = 64)
+    private String password;
+
+    @Column(name = "first_name", nullable = false, length = 20)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 20)
+    private String lastName;
+
+    @Column(nullable = false, length = 10)
+    private String contact;
+}
